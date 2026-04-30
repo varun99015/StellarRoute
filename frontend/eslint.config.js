90317be18bb56acf3c4b7a1081476dfcdfab1416
Varun S <varu99015@gmail.com>
2026-04-30 10:48:11 +0530

    Fixed eslint errors

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, loadEnv } from 'vite';
 
 export default [
   { ignores: ['dist'] },
